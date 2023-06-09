2faeb9b38ab9d650153b74b2e5bff39a3eddd9b5
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-06-09 11:11:58 -0600

    validation help

---
+++ Controllers/Validations.cs
@@ -0,0 +1,54 @@
+using Models.DataBaseContext;
+using System.Threading.Tasks;
+
+namespace Inventario.Controllers;
+
+public class Validations
+{
+
+    private readonly WareHouseDataContext _context;
+
+    public Validations(WareHouseDataContext Context)
+    {
+        _context = Context;
+    }
+
+    public bool IsUserValid(int idUser)
+    {
+        try
+        {
+            var query = (from U in _context.Users
+                         where U.Enable && U.UserId == idUser
+                         select U
+                            ).FirstOrDefault();
+            if (query != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (Exception f)
+        {
+            Console.WriteLine(f.Message);
+            return false;
+        }
+    }
+
+    public string HashPassword(string Password)
+    {
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception f)
+        {
+            Console.WriteLine(f.Message);
+            return string.Empty;
+        }
+    }
+
+
+}
